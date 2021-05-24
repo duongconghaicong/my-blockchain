1f82bf8830e11b7b3ead24f5fa55094276586ccc
Toan Pham <toanpham@Toans-MacBook-Air.local>
2021-05-24 22:34:13 +0700

    Created a new type

--- vue/src/views/Types.vue
+++ vue/src/views/Types.vue
@@ -2,6 +2,7 @@
 	<div>
 		<div class="container">
 			<!-- this line is used by starport scaffolding # 4 -->
+		<SpType modulePath="hello.planet.planet" moduleType="Post"  />
 		</div>
 	</div>
 </template>
